2e3a8bc0532abbb4aa9c8be3c2f4eadc3218924d
Mobinapournemat <mobina.poornemat@gmail.com>
2022-11-11 20:09:49 +0330

    implement command line interface

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 from math import Calculator
 
+
 calc = Calculator()
 
-print("Please Choose your operation: 1.Add  2.Subtraction   3.Multiplication    4.Division")
-op = 
+print("Please enter the number of your operation: 1.Add  2.Subtraction   3.Multiplication    4.Division")
+op = int(input())
+print("Enter your first number:")
+a = float(input())
+print("Enter your second number:")
+b = float(input())
